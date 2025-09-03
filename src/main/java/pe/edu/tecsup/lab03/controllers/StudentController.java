b6534ba6874e6061fb23bf3ecb365cb3c1693831
Zair Trivino <trivinoszair@gmail.com>
2025-09-02 22:48:52 -0500

    Modificacion de StudentController en sprint-2

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -2,9 +2,10 @@ package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
     public void registrarEstudiante() {
-        System.out.println("Estudiante registrado correctamente.");
+        System.out.println("✅ Estudiante registrado correctamente desde sprint-2.");
     }
     public void listarEstudiantes() {
-        System.out.println("Listado de estudiantes desde el nuevo StudentController.");
+        System.out.println("📋 Listado de estudiantes actualizado.");
     }
 }
+
